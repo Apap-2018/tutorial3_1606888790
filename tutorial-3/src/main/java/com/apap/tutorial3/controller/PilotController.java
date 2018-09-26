561a40812e087463b3b165c5bb3455b93254e1e9
Fany Azka Hidayatulloh <azkahebat12@gmail.com>
2018-09-26 20:08:07 +0700

    Fitur Latihan 1

--- tutorial-3/src/main/java/com/apap/tutorial3/controller/PilotController.java
+++ tutorial-3/src/main/java/com/apap/tutorial3/controller/PilotController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -42,4 +43,17 @@ public class PilotController {
 		return "viewall-pilot";
 	}
 
+    @RequestMapping("/pilot/view/license-number/{licenseNumber}")
+	public String viewPath(@PathVariable String licenseNumber, Model model) {
+        PilotModel archieve = pilotService.getPilotDetailByLicenseNumber(licenseNumber);
+        if (archieve!=null) {
+            model.addAttribute("pilot", archieve);
+            return "view-pilot";
+        }else {
+            String message = "Pilot dengan License Number "+licenseNumber+" tidak ditemukan";
+            model.addAttribute("errorMessage",message);
+            return "error";
+        }
+	}
+	
 }
